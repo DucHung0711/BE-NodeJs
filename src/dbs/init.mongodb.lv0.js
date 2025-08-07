5abe74a39d1ed36b88ea40e7d162fd515cda87f5
DucHung0711 <hnguyenduchung014@gmail.com>
2025-08-07 21:20:38 +0700

    Section3: check active connect and memory usege

---
+++ src/dbs/init.mongodb.lv0.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const connectString = `mongodb://localhost:27017/shopDEV`
+mongoose.connect(connectString).then( _ => console.log(`Connected Mongodb Success`))
+.catch( err => console.log(`Error Connect!`))
+
+module.exports = mongoose 
