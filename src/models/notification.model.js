0a653d83408230abad76779c48bd53e045f8d028
ECO1161_HUNGND <>
2025-12-01 17:13:20 +0700

    Section 42: Done code notification

---
+++ src/models/notification.model.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const { model, Schema } = require('mongoose')
+
+const DOCUMENT_NAME = 'Notification'
+const COLLECTION_NAME = 'Notifications'
+
+// ORDER-001: order successful
+// ORDER-002: order failed
+// PROMOTION-001: new Promotion
+// SHOP-001: new product by user following
+
+const notificationSchema = new Schema({
+    noti_type: { type: String, enum: ['ORDER-001', 'ORDER-002', 'PROMOTION-001', 'SHOP-001'], require: true },
+    noti_senderId: { type: Schema.Types.ObjectId, require: true, ref: 'Shop' },
+    noti_receivedId: { type: Number, require: true },
+    noti_content: { type: String, require: true },
+    noti_options: { type: Object, default: {} }
+}, {
+    collection: COLLECTION_NAME,
+    timestamps: true
+})
+
+module.exports = {
+    notification: model(DOCUMENT_NAME, notificationSchema)
+}
