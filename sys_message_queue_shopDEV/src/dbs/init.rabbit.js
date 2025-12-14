ea50d57d345b5b1940db5241e7e6d2dc13f9b6ef
DucHung0711 <hnguyenduchung014@gmail.com>
2025-12-15 00:05:57 +0700

    Section 45: Create sys rabbitmq

---
+++ sys_message_queue_shopDEV/src/dbs/init.rabbit.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const amqp = require('amqplib')
+
+const connectToRabbitMQ = async () => {
+    try {
+        const connection = await amqp.connect('amqp://localhost')
+        if (!connection) throw new Error('Failed to connect to RabbitMQ')
+
+        const channel = await connection.createChannel()
+        // const queue = 'shop_queue'
+        // channel.assertQueue(queue, { durable: true })
+        // console.log('Waiting for messages in %s. To exit press CTRL+C', queue)
+        // channel.consume(queue, (msg) => {
+        //     const data = JSON.parse(msg.content.toString())
+        //     console.log('Received message:', data)
+        // })
+
+        return { channel, connection }
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error)
+    }
+}
+
+const connectToRabbitMQForTest = async () => {
+    try {
+        const { channel, connection } = await connectToRabbitMQ()
+
+        const queue = 'shop_queue'
+        const message = 'Hello from RabbitMQ'
+        await channel.assertQueue(queue, { durable: true })
+        channel.sendToQueue(queue, Buffer.from(message))
+        console.log('Message sent to queue:', message)
+
+        // Wait a bit to ensure message is sent before closing
+        await new Promise(resolve => setTimeout(resolve, 100))
+
+        // close connection
+        await connection.close()
+
+        return true
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error)
+        throw error
+    }
+}
+
+module.exports = {
+    connectToRabbitMQ,
+    connectToRabbitMQForTest
+}
